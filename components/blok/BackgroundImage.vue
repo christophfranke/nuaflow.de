28c6e4da1716ce751008be8b19b016b19badb1b1
Christoph Franke <krito@krito.de>
2019-11-20 22:28:39 +0100

    fix vigniette effect (broken by last commit)

--- components/blok/BackgroundImage.vue
+++ components/blok/BackgroundImage.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-editable="blok" :style="image" class="outer" ref="outer">
+    <div class="vignette"></div>
     <div class="inner">
       <component :key="blok._uid" v-for="blok in blok.content" :blok="blok" :is="blok.component | dashify" />
     </div>
@@ -40,7 +41,15 @@ export default {
   min-height: 100vh;
   .inner {
     width: 100%;
+  }
+  .vignette {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     background: radial-gradient(circle at center, transparent, rgba(255, 255, 255, 0.1) 50%, rgba(0, 0, 0, 0.7));
+    pointer-events: none;
   }
   .arrow {
     position: absolute;
