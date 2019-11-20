5577faa1bc7f6788ed8c7ba51cc378fe1a568388
Christoph Franke <krito@krito.de>
2019-11-20 22:17:31 +0100

    fixed overflowing behavior and scroll arrow of background image

--- components/blok/BackgroundImage.vue
+++ components/blok/BackgroundImage.vue
@@ -1,6 +1,5 @@
 <template>
-  <div v-editable="blok" :style="image" class="outer">
-    <img :src="blok.image" ref="image" class="image">
+  <div v-editable="blok" :style="image" class="outer" ref="outer">
     <div class="inner">
       <component :key="blok._uid" v-for="blok in blok.content" :blok="blok" :is="blok.component | dashify" />
     </div>
@@ -22,8 +21,8 @@ export default {
 
   methods: {
     scrollToNextPage() {
-      const imageTop = this.$refs.image.offsetTop
-      const imageHeight = this.$refs.image.offsetHeight
+      const imageTop = this.$refs.outer.offsetTop
+      const imageHeight = this.$refs.outer.offsetHeight
       window.scrollTo({
         top: imageTop + imageHeight,
         behavior: 'smooth'
@@ -34,23 +33,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.image {
-  display: block;
-  width: 100%;
-  height: auto;
-  max-height: 100vh;
-  visibility: hidden;
-}
 .outer {
   position: relative;
   background-size: cover;
   background-position: center center;
+  min-height: 100vh;
   .inner {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    width: 100%;
     background: radial-gradient(circle at center, transparent, rgba(255, 255, 255, 0.1) 50%, rgba(0, 0, 0, 0.7));
   }
   .arrow {
